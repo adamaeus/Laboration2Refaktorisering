142fde8f1c78682adbb6ec6f1d6f080ed62f656d
nicke <niklas.andersson91@hotmail.com>
2023-02-08 16:48:58 +0100

    Merge av Niklas GUI och vår lab

--- CarController.java
+++ CarController.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 /*
-* This class represents the Controller part in the MVC pattern.
+* This class represents the Controller part in the MVC pattern
 * It's responsibilities is to listen to the View and responds in a appropriate manner by
 * modifying the model state and the updating the view.
  */
